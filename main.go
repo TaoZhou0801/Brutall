c2512d79df9bd559a48781562ed94565db997056
TaoZhou0801 <51364446+TaoZhou0801@users.noreply.github.com>
2021-09-26 15:50:40 -0400

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ var testlist = [][2]string{{"default", "1234567"}, {"admin", "1q2w3e4r"}, {"root
 var ul = []string{"admin", "zhoutao"}
 var pl = []string{"zhoutao"}
 var cl = [][2]string{{"test", "123456"}, {"root", "1q2w3e4r"}, {"admin", "1q2w3e4r"}, {"root", "654321"}}
-var cl_s = [][2]string{{"test", "123456"}, {"zhoutao@dp.com", "Lih,iiehwyas3!"}}
+var cl_s = [][2]string{{"test", "123456"}, {"zhoutao@dp.com", "1q2w3e4r"}}
 var snmplist = []string{"public", "private", "123456", "admin"}
 
 func main() {
